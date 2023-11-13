621f2d37577df2f20b3952635cfff9e74095d7e3
algernon-A <aeneas@aloku.net>
2023-11-14 10:34:33 +1100

    Add project files.

---
+++ Code/Mod.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="Mod.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// </copyright>
+
+namespace FreeRangeCamera
+{
+    using Colossal.IO.AssetDatabase;
+    using Colossal.Logging;
+    using Game;
+    using Game.Modding;
+    using Game.Serialization;
+
+    /// <summary>
+    /// The base mod class for instantiation by the game.
+    /// </summary>
+    public sealed class Mod : IMod
+    {
+        /// <summary>
+        /// The mod's default name.
+        /// </summary>
+        public const string ModName = "Free Range Camera";
+
+        /// <summary>
+        /// Gets the mod's active log.
+        /// </summary>
+        internal static ILog Log { get; private set; }
+
+        /// <summary>
+        /// Called by the game when the mod is loaded.
+        /// </summary>
+        public void OnLoad()
+        {
+            // Initialize logger.
+            Log = LogManager.GetLogger(ModName);
+            Log.Info("loading");
+
+            // Apply harmony patches.
+            new Patcher("algernon-FreeRangeCamera", Log);
+        }
+
+        /// <summary>
+        /// Called by the game when the game world is created.
+        /// </summary>
+        /// <param name="updateSystem">Game update system.</param>
+        public void OnCreateWorld(UpdateSystem updateSystem)
+        {
+            Log.Info("starting OnCreateWorld");
+
+            // Don't do anything if Harmony patches weren't applied.
+            if (Patcher.Instance is null || !Patcher.Instance.PatchesApplied)
+            {
+                Log.Critical("Harmony patches not applied; aborting system activation");
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Called by the game when the mod is disposed of.
+        /// </summary>
+        public void OnDispose()
+        {
+            Log.Info("disposing");
+
+            // Revert harmony patches.
+            Patcher.Instance?.UnPatchAll();
+        }
+    }
+}
---
+++ Code/Patches/CameraControllerPatches.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="CameraControllerPatches.cs" company="algernon (K. Algernon A. Sheppard)">
+// Copyright (c) algernon (K. Algernon A. Sheppard). All rights reserved.
+// </copyright>
+
+namespace FreeRangeCamera
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using Game;
+    using Game.Simulation;
+    using HarmonyLib;
+
+    /// <summary>
+    /// Harmony patches for <see cref="CameraController"/> to implement per-tile cost limits.
+    /// </summary>
+    [HarmonyPatch(typeof(CameraController))]
+    internal static class CameraControllerPatches
+    {
+        /// <summary>
+        /// Harmony transpiler for <see cref="CameraController.UpdateCamera"/> to override map edge bounds check.
+        /// </summary>
+        /// <param name="instructions">Original ILCode.</param>
+        /// <param name="original">Method being patched.</param>
+        /// <returns>Modified ILCode.</returns>
+        [HarmonyPatch("UpdateCamera")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> UpdateStatusTranspiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
+        {
+            Patcher.Instance.Log.Info("transpiling " + original.DeclaringType + '.' + original.Name);
+
+            // Look for call to this.
+            MethodInfo getBounds = AccessTools.Method(typeof(TerrainUtils), nameof(TerrainUtils.GetBounds));
+
+
+            // Parse instructions.
+            IEnumerator<CodeInstruction> instructionEnumerator = instructions.GetEnumerator();
+            while (instructionEnumerator.MoveNext())
+            {
+                CodeInstruction instruction = instructionEnumerator.Current;
+
+                // Look for call to TerrainUtils.GetBounds.
+                if (instruction.Calls(getBounds))
+                {
+                    Patcher.Instance.Log.Debug("found call to TerrainUtils.GetBounds");
+
+                    // Drop instruction argument and duplicate previous value on stack (m_Pivot).
+                    yield return new CodeInstruction(OpCodes.Pop);
+                    yield return new CodeInstruction(OpCodes.Dup);
+
+                    continue;
+                }
+
+                yield return instruction;
+            }
+        }
+    }
+}
